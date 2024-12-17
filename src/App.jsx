e7bf9a268cbee98c3dbbb6fe7aa231931bd31cd6
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-12-17 07:17:10 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header';
+import About from './components/About';
+import Contact from './components/Contact';
 
+    <div className="App">
+      <Header />
+      <div className="main-content">
+        <About />
+        <Contact />
+      </div>
+    </div>
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
 function App() {
   const [count, setCount] = useState(0)
 
