8767287fa13c09d6bd0f9962f34e688a2f9c1474
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-12-19 10:31:14 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header';
-import About from './components/About';
-import Contact from './components/Contact';
+
 
     <div className="App">
       <Header />
